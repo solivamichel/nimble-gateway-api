5476bde481922e93a4373b6d08753fcb2b552d93
Michel Soliva <michelsolivadev@gmail.com>
2025-10-20 11:14:57 -0300

    chore: configuração inicial do projeto com Spring Boot 3.3.3 e Java 17

---
+++ src/main/java/com/nimble/gateway/NimbleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nimble.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbleGatewayApplication.class, args);
+	}
+
+}
